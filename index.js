b64c25aa4139d133db4261629f9624708644586c
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-07-07 07:06:23 +0600

    add doctors and all doctors page added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,11 +10,77 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Base Route
-app.get('/', (req, res) => {
-    res.send('Hospital Management System Server is running...');
+// MongoDB Atlas
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log("🏥 CarePulse Database Successfully Connected to MongoDB Atlas! 🎉");
+
+
+        const db = client.db("hms");
+        const usersCollection = db.collection("users");
+        const doctorsCollection = db.collection("doctor")
+        const appointmentsCollection = db.collection("appointments");
+
+
+        app.post("/api/doctors", async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        });
+
+        // GET all doctors
+        app.get("/api/doctors", async (req, res) => {
+            const result = await doctorsCollection.find().toArray();
+            res.send(result);
+        });
+
+        // APPROVE doctor
+        app.patch("/api/doctors/approve/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await doctorsCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { status: "approved" } }
+            );
+            res.send(result);
+        });
+
+        // DELETE doctor
+    
+        app.delete("/api/doctors/:id", async (req,res)=>{
+            try{
+                const id = req.params.id;
+                const query = {_id:new ObjectId(id)}
+                const result = await doctorsCollection.deleteOne(query);
+                res.send(result)
+            }catch(error){
+                res.status(500).send({ success:false , message: error.message});
+            }
+        })
+
+
+
+        app.get("/", (req, res) => {
+            res.send("CarePulse Hospital Management Server is Running...");
+        });
+
+    } catch (error) {
+        console.error("❌ Database connection error:", error);
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`🚀 CarePulse Server is spinning on port: ${port}`);
 });
